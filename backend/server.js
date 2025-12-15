1d119003be07ab9b0549bdd40935f28c96f6bfa3
mz0018 <martinezhanzmenzi@gmail.com>
2025-12-15 08:57:31 +0800

    used single backend port instead of 2

--- backend/server.js
+++ backend/server.js
@@ -7,5 +7,5 @@ connectDB();
 const PORT = process.env.PORT || 5001;
 
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+  console.log(`Backend running on port ${PORT}`);
 });
