5d4643ae6abeeb42171ba2226cf860b9f5bf40a7
mz0018 <martinezhanzmenzi@gmail.com>
2025-12-15 07:31:01 +0000

    production ready initial testing

--- admin/src/main.jsx
+++ admin/src/main.jsx
@@ -1,21 +1,29 @@
-import { StrictMode, Suspense, lazy } from 'react'
+import { StrictMode, lazy } from 'react'
 import { createRoot } from 'react-dom/client'
-import { createBrowserRouter, Router, RouterProvider } from 'react-router-dom'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import './index.css'
 import App from './App.jsx'
 import { AuthProvider } from './contexts/AuthContext.jsx'
 
-const SuperAdminDashboard = lazy(() => import('./components/SuperAdminDashBoard.jsx'));
+const SuperAdminDashboard = lazy(() =>
+  import('./components/SuperAdminDashBoard.jsx')
+)
 
-const router = createBrowserRouter([
-  { path: "/", element: <App /> },
-  { path: "/superadmin", element: <SuperAdminDashboard /> }
-])
+const router = createBrowserRouter(
+  [
+    { path: "/", element: <App /> },
+    { path: "/superadmin", element: <SuperAdminDashboard /> },
+  ],
+  {
+    basename: "/auth",
+  }
+)
 
 createRoot(document.getElementById('root')).render(
-  <AuthProvider>
-    <StrictMode>
+  <StrictMode>
+    <AuthProvider>
       <RouterProvider router={router} />
-    </StrictMode>
-  </AuthProvider>,
-);
+    </AuthProvider>
+  </StrictMode>
+)
+
